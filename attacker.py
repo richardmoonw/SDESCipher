2990277e7da3760ebfff01395c643631c4c52378
Ricardo <ricardo@themoondev.com>
2020-09-01 22:34:25 -0600

    Brute-force attack finished. Some refactory is needed

--- attacker.py
+++ attacker.py
@@ -1,24 +1,46 @@
 from utilities import decimal_to_binary
 from encryptor import transform
 
-DECRYPTION = "decryption"
+ENCRYPTION = "encryption"
 
 def brute_force(file):
 	# Open the file specified by the user and read all the records.
 	fhandler = open(file, "r")
 
-	# Try to find the correct key for all the pairs of ciphertext and plaintext.
-	first_pt = fhandler.readline()[:8]
+	# Find the correct keys for the first pair of ciphertext and plaintext.
+	first_line = fhandler.readline()
+	first_pt = first_line[:8]
+	first_ct = first_line[9:17]
 	possible_keys = []
 	for x in range(pow(2,10)):
 		key = decimal_to_binary(x)[-10:]
 		key = ''.join([str(elem) for elem in key])
-		output = transform(DECRYPTION, key, first_pt, False)
+		output = transform(ENCRYPTION, key, first_pt, False)
 
-		if fhandler.readline()[9:17] == output:
+		if first_ct == output:
 			possible_keys.append(key)
-	
+
 	print(possible_keys)
+	
+	key_found = False
+	final_key = ""
+	for key in possible_keys:
+		for line in fhandler:
+			plaintext = line[:8]
+			real_ciphertext = line[9:17]
+			ciphertext = transform(ENCRYPTION, key, plaintext, False)
+			# print(line)
+			# print("Real cipher {} and cipher {}".format(real_ciphertext,ciphertext))
 
-
-	# If the key was not the correct, try the next one.
+			if real_ciphertext != ciphertext:
+				key_found = False
+				final_key = ""
+				break
+			elif real_ciphertext == ciphertext and key_found is False:
+				key_found = True
+				final_key = key
+				continue
+		if key_found is True:
+			break
+	
+	print("The perfect key is {}".format(final_key))
